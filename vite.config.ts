6be2c758789dca4f3e9f3484fbeb90eff28af3b0
Mohamed Samir <mosamircs@gmail.com>
2025-09-10 22:50:00 +0300

    solved

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/tailor-one/', // Replace with your actual repo name
   server: {
     host: "::",
     port: 8080,
